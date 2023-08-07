18755d5de1184b04e5ba501e3bea51fb1e7b4343
iamdebobrota <iamdebobrota@gmail.com>
2023-08-07 12:52:38 +0530

    Initialize project using Create React App

---
+++ data/folderData.js
@@ -0,0 +1,13 @@
+const explorer = {
+  name: "root",
+  isFolders: true,
+  items: [
+    { 
+        name: "public", 
+    isFolders: true,
+     items: [
+        name: "public nested 1
+     ] 
+    },
+],
+};
